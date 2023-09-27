747aac9da97be405bd148c91d0c6cdfe372a828c
Vít Němec <vitek2004@seznam.cz>
2023-09-27 09:57:45 +0200

    upgrade

--- TestingSpeed/Program.cs
+++ TestingSpeed/Program.cs
@@ -1,72 +1,78 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace TestingSpeed
+namespace TestingSpeed;
+
+internal class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static Stopwatch stopky = new Stopwatch();
-        static Data[] dat = new Data[50000];
-        static void Main(string[] args)
+        var stopwatch = new Stopwatch();
+        var dataArray = new Data[50000];
+
+        // Creation of Data structs
+        stopwatch.Start();
+        for (var i = 0; i < dataArray.Length; i++)
         {
-            //Vytvoření s jednoduchou inicializací
-            stopky.Start();
-            for (int i = 0; i < dat.Length; i++)
-            {
-                dat[i] = new Data(new byte[16]);
-            }
-            stopky.Stop();
-            Console.WriteLine("Při jednoduchém tvoření uběhlo: " + stopky.Elapsed.TotalMilliseconds);
-            stopky.Reset();
-
-            //Vytvoření pomocí Guidů (které téměř zaručeně vytvoří unikátní hodnotu)
-            stopky.Start();
-            for (int i = 0; i < dat.Length; i++)
-            {
-                dat[i] = new Data(new Guid().ToByteArray());
-            }
-            stopky.Stop();
-            Console.WriteLine("Při GUIDovém tvoření uběhlo: " + stopky.Elapsed.TotalMilliseconds);
-            stopky.Reset();
-
-            //Porovnávání prvního elementu se všemi ostatními
-            stopky.Start();
-            for (int i = 0; i < dat.Length; i++)
-            {
-                dat[0].Equals(dat[i]);
-            }
-            stopky.Stop();
-            Console.WriteLine("Při porovnávání prvního s každým uběhlo: " + stopky.Elapsed.TotalMilliseconds);
-            stopky.Reset();
+            dataArray[i] = new Data(new byte[16]);
         }
+
+        stopwatch.Stop();
+        Console.WriteLine("Elapsed time during creation of data structs: " + stopwatch.Elapsed.TotalMilliseconds +
+                          "ms");
+        stopwatch.Reset();
+
+        // Creation of GUIDs
+        stopwatch.Start();
+        for (var i = 0; i < dataArray.Length; i++)
+        {
+            dataArray[i] = new Data(new Guid().ToByteArray());
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine("Elapsed time during creation of GUIDs: " + stopwatch.Elapsed.TotalMilliseconds + "ms");
+        stopwatch.Reset();
+
+        // Comparison of the first element in dataArray against every element in the dataArray
+        stopwatch.Start();
+        foreach (var dataUnit in dataArray)
+        {
+            dataArray[0].Equals(dataUnit);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine(
+            "Elapsed time during the comparison of the first element against every other element in the dataArray: "
+            + stopwatch.Elapsed.TotalMilliseconds + "ms");
+        stopwatch.Reset();
+    }
+}
+
+internal readonly struct Data : IEquatable<Data>
+{
+    private readonly byte[] _buffer;
+
+    public Data(byte[] bytes)
+    {
+        _buffer = bytes;
     }
 
-    struct Data : IEquatable<Data>
+    public override bool Equals(object obj)
     {
-        byte[] buffer;
-
-        //Equals
-        public override bool Equals(object obj)
-        {
-            if (obj is not Data) return false;
-            return Equals((Data) obj);
-        }
-        public bool Equals(Data other)
-        {
-            return buffer.Equals(other.buffer);
-        }
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(buffer.ToString());
-        }
-
-        //Operators
-        public static bool operator ==(Data d1, Data d2) => d1.Equals(d2);
-        public static bool operator !=(Data d1, Data d2) => !(d1.Equals(d2));
-
-        public Data (byte[] bytes)
-        {
-            buffer = bytes;
-        }
+        return obj is Data data && Equals(data);
     }
+
+    public bool Equals(Data other)
+    {
+        return _buffer.Equals(other._buffer);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_buffer.ToString());
+    }
+
+    //Operators
+    public static bool operator ==(Data d1, Data d2) => d1.Equals(d2);
+    public static bool operator !=(Data d1, Data d2) => !(d1.Equals(d2));
 }
